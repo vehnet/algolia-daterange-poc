8943dffaf62df78e7d5fc18e105a4f2c307fd3dc
cannblw <edgar.95.12@gmail.com>
2022-02-17 16:28:18 +0100

    Re-estructure App.tsx and add date filter

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,26 @@
 import React from 'react';
 
 import { useHits } from "react-instantsearch-hooks";
+import { AlgoliaDateRange } from './AlgoliaDateRange';
 
 const App: React.FC = () => {
   const hits = useHits();
 
   return (
     <div className="container">
-      <strong>Results ({hits.results?.nbHits} items):</strong>
-
-      <div className="results">
-        {hits.hits.map(row =>
-          <div className="row">{row.title}, {row.author}. <strong>Created at:</strong> {row.created_at} ({row.created_at_i})</div>
-        )}
-      </div>
+      <section>
+        <strong>Filters:</strong>
+        <AlgoliaDateRange attribute="created_at_i" />
+      </section>
+      
+      <section>
+        <strong>Results ({hits.results?.nbHits} items):</strong>
+        <div className="results">
+          {hits.hits.map(row =>
+            <div className="row">{row.title}, {row.author}. <strong>Created at:</strong> {row.created_at} ({row.created_at_i})</div>
+          )}
+        </div>
+      </section>
     </div>
   )
 };
